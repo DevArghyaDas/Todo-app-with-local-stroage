6cc082d3456deaeb3e8295ed38858c179dcbd041
Arghya Das <mrarghyadasdev@gmail.com>
2025-06-26 09:04:41 +0530

    added meta-data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import AddTodoFrom from "@/components/myUi/AddTodoFrom";
 import ShowAllTodo from "@/components/myUi/ShowAllTodo";
+import { Metadata } from "next";
+
+export const metaData: Metadata = {
+    title: "Todo | Home",
+    description: "homepage of todo app",
+};
 
 const page = () => {
     return (
